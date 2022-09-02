7a7c8c2d0044c770387fc35c52bf0b491c2da03f
r@jesh <wadoderajesh@gmail.com>
2022-09-02 16:21:31 +0530

    database add

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from  flask import Flask,render_template,request
 from function import california
+import mysql.connector
 app=Flask(__name__)
 
 @app.route("/")
@@ -21,10 +22,27 @@ def user():
     median_income=float(data["median_income"])
     ocean_proximity=(data["ocean_proximity"])
     
+   
     
     price=california(longitude, latitude,housing_median_age, total_rooms, total_bedrooms, population, households, median_income, ocean_proximity)
     house_price=price.predict()
     print(house_price)
+    
+    conn = mysql.connector.connect( host= 'localhost',
+                                    database = 'california',
+                                    user= 'root',
+                                    password = 'jaymauli')
+
+    cursor = conn.cursor()
+
+    query = "INSERT INTO house(longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income,ocean_proximity,house_price) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    data = (longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income,ocean_proximity,house_price)
+
+    cursor.execute(query,data)
+
+    conn.commit()
+    conn.close()
+    
 
     return render_template("index.html",final_price=house_price)
 
